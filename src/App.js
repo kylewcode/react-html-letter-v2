2910467db70af172807cea4cba400a2772a587f1
Kyle Williams <williams.kyleg@gmail.com>
2020-11-22 15:00:13 -0500

    basic app completed via create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,313 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "./Modal";
+import Display from "./Display";
+import "./App.css";
 
 function App() {
+  const states = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Columbia",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming",
+  ];
+  const statesJSX = [];
+  for (const [index, value] of states.entries()) {
+    statesJSX.push(
+      <option key={index} value={value}>
+        {value}
+      </option>
+    );
+  }
+
+  const initState = {
+    firstName: "Kyle",
+    lastName: "Williams",
+    streetNumber: "10308",
+    streetName: "Kennebec Ct",
+    aptSuite: "",
+    city: "Orlando",
+    stateName: "Florida",
+    zipcode: "32817",
+    date: "2020-11-17",
+    isFemale: false,
+    isMarried: false,
+    status: "fillingOutForm",
+    dataStore: {},
+  };
+
+  const [state, setState] = useState(() => initState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Sending request...");
+    // Stop request here because I need to remove my API key to deploy to Heroku by creating a new git brang called main
+
+    // const requestUrl = `${baseUrl}${state.streetNumber}%20${state.streetName}%20${state.stateName},%20${state.city}`;
+    // createRequest(requestUrl);
+
+    // Now I need to trigger the state change without the XHR request.
+    setState({ ...state, status: "formConfirmed" });
+  }
+
+  /* Address Validation */
+
+  const baseUrl =
+    "http://api.positionstack.com/v1/forward?access_key=POSSTACK_APIKEY&query=";
+
+  // Server Request
+  const updateUISuccess = function (response) {
+    const data = JSON.parse(response).data;
+    setState({ ...state, dataStore: data, status: "formSubmitted" });
+  };
+
+  const updateUIError = function (error) {
+    console.log(error);
+  };
+
+  const responseMethod = function (httpRequest) {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        updateUISuccess(httpRequest.responseText);
+      } else {
+        updateUIError(httpRequest.status + ": " + httpRequest.responseText);
+      }
+    }
+  };
+
+  const createRequest = function (url) {
+    const httpRequest = new XMLHttpRequest(url);
+    httpRequest.addEventListener("readystatechange", () => {
+      responseMethod(httpRequest);
+    });
+    httpRequest.open("GET", url);
+    httpRequest.send();
+  };
+
+  const formElement = (
+    <div className="user-inputs">
+      <h1>Formal Letter Generator</h1>
+      <p className="required-field">Required fields are marked with *</p>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          <li className="bg-bright">
+            <label htmlFor="user-first-name">Enter your first name</label>
+            <input
+              type="text"
+              name="first-name"
+              id="user-first-name"
+              value={state.firstName}
+              placeholder="First name*"
+              // required
+              onChange={(e) =>
+                setState({ ...state, firstName: e.target.value })
+              }
+            />
+          </li>
+          <li className="bg-bright">
+            <label htmlFor="user-last-name">Enter your last name</label>
+            <input
+              type="text"
+              name="last-name"
+              id="user-last-name"
+              value={state.lastName}
+              placeholder="Last name*"
+              // required
+              onChange={(e) => setState({ ...state, lastName: e.target.value })}
+            />
+          </li>
+          <li className="bg-light">
+            <fieldset>
+              <legend>Enter your mailing address</legend>
+              <input
+                type="text"
+                name="street-number"
+                id="user-street-number"
+                value={state.streetNumber}
+                placeholder="Street number*"
+                required
+                pattern="\d+"
+                onChange={(e) =>
+                  setState({ ...state, streetNumber: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                name="street-name"
+                id="user-street-name"
+                value={state.streetName}
+                placeholder="Street Name*"
+                required
+                onChange={(e) =>
+                  setState({ ...state, streetName: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                name="apt-suite"
+                id="user-apt-suite"
+                value={state.aptSuite}
+                placeholder="Apt/Suite #"
+                onChange={(e) =>
+                  setState({ ...state, aptSuite: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                name="city"
+                id="user-city"
+                value={state.city}
+                placeholder="City*"
+                required
+                onChange={(e) => setState({ ...state, city: e.target.value })}
+              />
+              <select
+                name="state"
+                id="state-select"
+                required
+                onChange={(e) =>
+                  setState({ ...state, stateName: e.currentTarget.value })
+                }
+              >
+                <option value="">Select state*</option>
+                {statesJSX}
+              </select>
+              <input
+                type="text"
+                name="zipcode"
+                id="user-zipcode"
+                value={state.zipcode}
+                placeholder="Zipcode*"
+                required
+                pattern="\d{5}"
+                onChange={(e) =>
+                  setState({ ...state, zipcode: e.target.value })
+                }
+              />
+            </fieldset>
+          </li>
+          <li className="bg-bright">
+            <label htmlFor="date-input">Enter Date*</label>
+            <input
+              type="date"
+              name="date"
+              id="date-input"
+              // required
+              onChange={(e) => setState({ ...state, date: e.target.value })}
+            />
+          </li>
+          <li className="bg-light">
+            <p>Select Gender*</p>
+            <div>
+              <input
+                type="radio"
+                name="gender"
+                id="male"
+                // required
+                onClick={(e) => setState({ ...state, isFemale: false })}
+              />
+              <label htmlFor="male">Male</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="gender"
+                id="female"
+                // required
+                onClick={(e) => setState({ ...state, isFemale: true })}
+              />
+              <label htmlFor="female">Female</label>
+            </div>
+          </li>
+          <li className="bg-bright">
+            <p>Are you married?*</p>
+            <div>
+              <input
+                type="radio"
+                name="married"
+                id="married-yes"
+                // required
+                onClick={(e) => setState({ ...state, isMarried: true })}
+              />
+              <label htmlFor="married-yes">Yes</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="married"
+                id="married-no"
+                // required
+                onClick={(e) => setState({ ...state, isMarried: false })}
+              />
+              <label htmlFor="married-no">No</label>
+            </div>
+          </li>
+          <li>
+            <input type="submit" value="Submit" />
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {state.status === "fillingOutForm" && formElement}
+      {state.status === "formSubmitted" && (
+        <React.Fragment>
+          <Modal modalState={state} modalSetState={setState} />
+          {formElement}
+        </React.Fragment>
+      )}
+      {state.status === "formConfirmed" && (
+        <Display displayState={state} displaySetState={setState} />
+      )}
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,114 @@
+import React from "react";
+import "./App.css";
+
+function Modal({
+  modalSetState,
+  modalState: {
+    firstName,
+    lastName,
+    streetNumber,
+    streetName,
+    aptSuite,
+    city,
+    stateName,
+    zipcode,
+    date,
+    isFemale,
+    isMarried,
+    status,
+    dataStore,
+  },
+}) {
+  const childState = {
+    firstName,
+    lastName,
+    streetNumber,
+    streetName,
+    aptSuite,
+    city,
+    stateName,
+    zipcode,
+    date,
+    isFemale,
+    isMarried,
+    status,
+    dataStore,
+  };
+
+  // Access dataStore to grab the possible addresses and construct some radio buttons from them
+  // For every location that is returned
+  // Take the address and make it into a radio button to select
+  const addressJSX = [];
+  dataStore.forEach((location, key) => {
+    const address = `${location.number} ${location.street} ${location.locality} ${location.region_code} ${location.postal_code}`;
+    addressJSX.push(
+      <div key={key}>
+        <input
+          type="radio"
+          name="address-validate"
+          id={`valid-${key}`}
+          value={address}
+          /* Data attributes reflect JSON data */
+          data-address-number={location.number}
+          data-address-street={location.street}
+          data-address-locality={location.locality}
+          data-address-region-code={location.region_code}
+          data-address-zip-code={location.postal_code}
+        />
+        <label htmlFor={`valid-${key}`}>{address}</label>
+      </div>
+    );
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const radioNodeList = e.target.elements["address-validate"];
+    radioNodeList.forEach((node) => {
+      if (node.checked === true && node.defaultValue === "N/A") {
+        alert("Please enter a correct address and submit again.");
+        modalSetState({ ...childState, status: "fillingOutForm" });
+        return;
+      }
+      if (node.checked === true) {
+        modalSetState({
+          ...childState,
+          streetNumber: node.dataset.addressNumber,
+          streetName: node.dataset.addressStreet,
+          city: node.dataset.addressLocality,
+          stateName: node.dataset.addressRegionCode,
+          zipcode: node.dataset.addressZipCode,
+          status: "formConfirmed",
+        });
+      }
+    });
+  }
+
+  return (
+    <div>
+      <div className="modal">
+        <div className="modal-content">
+          <p>
+            These are the addresses in our records that match what you have
+            entered. Note that apartment or suite numbers are disregarded.
+            Please select one of the following:
+          </p>
+          <form onSubmit={handleSubmit}>
+            {addressJSX}
+            <div>
+              <input
+                type="radio"
+                name="address-validate"
+                id="valid-none"
+                value="N/A" // Maybe the / will cause an issue?
+              />
+              <label htmlFor="valid-none">I do not see my address here.</label>
+            </div>
+            <button type="submit">Submit Button</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
