1a38e12e07f6bf13a8234a42293a850fa55dc9cb
Kyle Williams <williams.kyleg@gmail.com>
2021-02-19 13:06:48 -0500

    test data successfully passed to proxy

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
 
 let whitelist = ['http://localhost:3000'];
 const corsOptions = {
