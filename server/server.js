9be2054ef50385e36283786ab9ab13c5724ead0d
Kyle Williams <williams.kyleg@gmail.com>
2021-02-19 13:00:33 -0500

    axios, cors setup for dev, test data request to proxy

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
-
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+let whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
-app.get('/', (req, res) => {
+app.options('/', cors(corsOptions));
+
+app.get('/', cors(corsOptions), (req, res) => {
   res.send('GET request to proxy');
 });
 
-app.post('/', (req, res) => {
+// cors() passed as middleware does not fix cors error
+app.post('/', cors(corsOptions), (req, res) => {
+  console.log(req.body);
   res.send('POST request to proxy');
 });
 
